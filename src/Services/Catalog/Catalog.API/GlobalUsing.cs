af196ffbd46e229ad382eb6f8df7f6d23e87e775
jayakumarvinayagam2023 <92949311+jayakumarvinayagamsixrules@users.noreply.github.com>
2024-04-03 00:33:19 +0530

    catalog product feature create product updated

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
+global using MediatR;
+global using Carter;
+global using Mapster;
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price 
+    ) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(
+    Guid Id );
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product.
+        // 1. Create Product entity from command object.
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        // 2. Save to database.        
+        // 3. return CreateProductResult result.
+        await Task.CompletedTask;
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
